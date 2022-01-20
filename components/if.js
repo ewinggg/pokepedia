a474d78ce332fb576ba816a82d3e8a2af6c4e194
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 01:09:35 +0700

    🧹Refactor: Updated If component

--- components/if.js
+++ components/if.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 const If = ({ children, condition }) => {
   if (!condition) return null
 
-  // Render children if condition is true
+  // Render children if condition is truthy.
   return children
 }
 
@@ -11,7 +11,8 @@ If.propTypes = {
   // PropTypes.node: Anything that can be rendered: numbers, strings, elements or an array.
   // PropTypes.element: A React element.
   children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]).isRequired,
-  condition: PropTypes.bool.isRequired,
+  condition: PropTypes.oneOfType([PropTypes.bool, PropTypes.oneOf([undefined])])
+    .isRequired,
 }
 
 export default If
