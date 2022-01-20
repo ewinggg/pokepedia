942ef0fa4dfba594a4a31430878178ade108502c
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 01:26:30 +0700

    🚀Feat: Create pokemon-list component

---
+++ components/pokemon-list.js
@@ -0,0 +1,34 @@
+import PropTypes from "prop-types"
+import If from "./if"
+import PokemonItem from "./pokemon-item"
+import styled from "@emotion/styled"
+
+const StyledList = styled.ul`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 25px;
+  flex-wrap: wrap;
+`
+
+const PokemonList = ({ pokemons }) => (
+  <If condition={pokemons?.length > 0}>
+    <StyledList>
+      {
+        // Iterate each pokemon from pokemons
+        pokemons.map((pokemon) => (
+          <PokemonItem pokemon={pokemon} key={pokemon.id} />
+        ))
+      }
+    </StyledList>
+  </If>
+)
+
+PokemonList.propTypes = {
+  pokemons: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.object),
+    PropTypes.array,
+  ]).isRequired,
+}
+
+export default PokemonList
