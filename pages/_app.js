1ffb9c89a24eb7bbcab6ec9ded3dc56a226255b8
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 01:36:37 +0700

    🧹Refactor: Configure ApolloProvider in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,13 @@
+import { ApolloProvider } from "@apollo/client"
+import client from "../graphql/client"
 import { Global } from "@emotion/react"
 import globalStyles from "../styles/global-styles"
 
 const App = ({ Component, pageProps }) => (
-  <>
+  <ApolloProvider client={client}>
     <Global styles={globalStyles} />
     <Component {...pageProps} />
-  </>
+  </ApolloProvider>
 )
 
 export default App
