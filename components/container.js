3d934edfd2cb2465e8b1cca6d41845a31c33bc19
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 15:02:31 +0700

    🚀Feat: Created container component

---
+++ components/container.js
@@ -0,0 +1,31 @@
+import PropTypes from "prop-types"
+import styled from "@emotion/styled"
+import media from "../styles/media"
+
+export const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+  align-items: center;
+  ${media.xs} {
+    align-items: flex-start;
+    max-width: 685px;
+  }
+  ${media.sm} {
+    max-width: 768px;
+  }
+  ${media.lg} {
+    max-width: 1024px;
+  }
+  margin: 25px auto;
+`
+
+const Container = ({ children }) => (
+  <StyledContainer>{children}</StyledContainer>
+)
+
+Container.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]).isRequired,
+}
+
+export default Container
