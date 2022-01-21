d150ac578a4232698225320d42dc8fb048b910d5
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 16:46:13 +0700

    🚀Feat: Crated custom hooks to fetch pokemons

---
+++ hooks/usePokemons.js
@@ -0,0 +1,17 @@
+import { useLazyQuery } from "@apollo/client"
+import { useEffect, useState } from "react"
+import { GET_POKEMONS } from "../graphql/query"
+
+const usePokemons = (initialPokemons) => {
+  const [pokemons, setPokemons] = useState(initialPokemons)
+  const [getPokemons, { data, loading, error }] = useLazyQuery(GET_POKEMONS)
+
+  useEffect(() => {
+    const newPokemons = data?.pokemons?.results
+    if (newPokemons) setPokemons((pokemons) => [...pokemons, ...newPokemons])
+  }, [data])
+
+  return { loading, error, pokemons, getPokemons }
+}
+
+export default usePokemons
