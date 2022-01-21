29a2d6b160026a948ece7a4f9cc1cea2f4b5ea77
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-22 01:20:14 +0700

    🚀Feat: Created custom hooks to fetch pokemon detail

---
+++ hooks/usePokemon.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@apollo/client"
+import { useEffect, useState } from "react"
+import { GET_POKEMON } from "../graphql/query"
+
+const usePokemons = (name) => {
+  const [pokemon, setPokemon] = useState({})
+  const { data, loading, error } = useQuery(GET_POKEMON, {
+    variables: { name },
+  })
+
+  useEffect(() => {
+    const { pokemon } = data ?? {}
+    if (pokemon) setPokemon(pokemon)
+  }, [data])
+
+  return { loading, error, pokemon }
+}
+
+export default usePokemons
