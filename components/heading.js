bde046fa3c35cf03648d0b26686b24dd4c5bc846
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 15:57:23 +0700

    🧹Refactor: Updated heading component

--- components/heading.js
+++ components/heading.js
@@ -1,24 +1,28 @@
 import PropTypes from "prop-types"
+import { forwardRef } from "react"
 import If from "./if"
 import styled from "@emotion/styled"
 
 const StyledHeading = styled.h1`
-  color: inherit;
+  font-size: 100%;
+  margin: 0;
 `
 
-const Heading = ({ children, level, style }) => {
+const Heading = forwardRef((props, ref) => {
+  const { children, level, style, ...rest } = props
+
   // Set level from h1 to h6
   // If level is not valid, fallback to h2
   const tag = `h${level < 7 ? level : 2}`
 
   return (
     <If condition={typeof children === "string"}>
-      <StyledHeading as={tag} className={style}>
+      <StyledHeading as={tag} ref={ref} className={style} {...rest}>
         {children}
       </StyledHeading>
     </If>
   )
-}
+})
 
 Heading.defaultProps = {
   style: "",
