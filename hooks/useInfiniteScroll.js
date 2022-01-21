3097cfe83f90930b42ebb45b139b7fc2acd5bad1
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 17:04:19 +0700

    🚀Feat: Created infinite scroll custom hooks

---
+++ hooks/useInfiniteScroll.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react"
+import { PER_PAGE, VARIABLES as variables } from "../graphql/constants"
+
+const useInfiniteScroll = (loading, callback) => {
+  let perPage = PER_PAGE
+
+  const scrollListener = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+    if (!loading && scrollTop + clientHeight >= scrollHeight) {
+      callback({ variables: { ...variables, offset: (perPage += PER_PAGE) } })
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollListener)
+
+    return () => window.removeEventListener("scroll", scrollListener)
+  }, [])
+}
+
+export default useInfiniteScroll
