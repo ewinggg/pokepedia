8459338adab17f0238892e2eeefd73d0bcb0f346
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 16:22:52 +0700

    🧹Refactor: Updated navbar routes

--- utils/routes.js
+++ utils/routes.js
@@ -1,8 +1,18 @@
+import Pokeball from "../components/pokeball"
+import World from "../components/world"
+
 const routes = [
   {
     id: 1,
-    path: "/pokemons",
+    path: "/",
+    name: "World",
+    icon: <World size={3} />,
+  },
+  {
+    id: 2,
+    path: "/collection",
     name: "My Pokemons",
+    icon: <Pokeball size={3} />,
   },
 ]
 
