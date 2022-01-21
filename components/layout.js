6ff5d0609e34d121aa551f81e3aef03967713e01
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-21 15:04:15 +0700

    🚀Feat: Created layout component

---
+++ components/layout.js
@@ -0,0 +1,24 @@
+import PropTypes from "prop-types"
+import Container from "./container"
+import Topbar from "./topbar"
+import styled from "@emotion/styled"
+
+export const StyledLayout = styled.div`
+  min-height: 100vh;
+  height: 100%;
+`
+
+const Layout = ({ children }) => (
+  <StyledLayout>
+    <Container>
+      <Topbar />
+      <main>{children}</main>
+    </Container>
+  </StyledLayout>
+)
+
+Layout.propTypes = {
+  children: PropTypes.oneOfType([PropTypes.node, PropTypes.element]).isRequired,
+}
+
+export default Layout
