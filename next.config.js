d124ccad0aad8bd642a1ec91500101b8eefe9451
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-22 20:15:30 +0700

    🧑‍💻Chore: PWA Setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
-module.exports = {
+const withPWA = require("next-pwa")
+
+const config = {
   images: {
     domains: ["raw.githubusercontent.com"],
   },
+  pwa: {
+    dest: "public",
+    disable: process.env.NODE_ENV === "development",
+  },
+
   reactStrictMode: true,
 }
+
+module.exports = withPWA(config)
