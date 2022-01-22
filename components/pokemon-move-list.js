6329d6cce6d187d57a8f08167cdd8ade9f8630c8
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-22 13:01:37 +0700

    🚀Feat: Created useCachedImage custom hooks

---
+++ components/pokemon-move-list.js
@@ -0,0 +1,38 @@
+/** @jsxImportSource @emotion/react */
+
+import PropTypes from "prop-types"
+import If from "./if"
+import List from "./list"
+import PokemonMoveItem from "./pokemon-move-item"
+import { css } from "@emotion/react"
+
+const listStyle = css`
+  display: flex;
+  justify-content: flex-start;
+  gap: 5px;
+  max-height: 412px;
+  overflow-y: scroll;
+  padding: 5px;
+`
+
+const PokemonMoveList = ({ pokemonMoves }) => (
+  <If condition={pokemonMoves?.length > 0}>
+    <List css={listStyle}>
+      {
+        // Iterate each ability from abilities
+        pokemonMoves?.map(({ move }) => (
+          <PokemonMoveItem pokemonMove={move} key={move.name} />
+        ))
+      }
+    </List>
+  </If>
+)
+
+PokemonMoveList.propTypes = {
+  pokemonMoves: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.object),
+    PropTypes.array,
+  ]).isRequired,
+}
+
+export default PokemonMoveList
