8aa101170d5cab32115db612b4c6a6ff3703c8b7
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-22 12:46:54 +0700

    🧹Refactor: Updated pokemon-list component

--- components/pokemon-list.js
+++ components/pokemon-list.js
@@ -1,11 +1,21 @@
+/** @jsxImportSource @emotion/react */
+
 import PropTypes from "prop-types"
 import If from "./if"
 import List from "./list"
 import PokemonItem from "./pokemon-item"
+import { css } from "@emotion/react"
+import media from "../styles/media"
+
+const listStyle = css`
+  ${media.sm} {
+    justify-content: space-between;
+  }
+`
 
 const PokemonList = ({ pokemons }) => (
   <If condition={pokemons?.length > 0}>
-    <List>
+    <List css={listStyle}>
       {
         // Iterate each pokemon from pokemons
         pokemons?.map((pokemon) => (
