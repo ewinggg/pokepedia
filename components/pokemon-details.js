9810d12fe6bb8efa3ca604bfafa9cffc54cca295
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-22 12:16:18 +0700

    🧹Refactor: Updated pokemon-details component

--- components/pokemon-details.js
+++ components/pokemon-details.js
@@ -4,10 +4,28 @@ import Image from "next/image"
 import PropTypes from "prop-types"
 import Heading from "../components/heading"
 import If from "./if"
-import { css } from "@emotion/react"
+import { css, Global } from "@emotion/react"
 import media from "../styles/media"
+import PokemonTypeList from "./pokemon-type-list"
+import useColors from "../hooks/useColors"
 
-const baseDetailsStyle = css`
+const columnStyle = css`
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`
+
+const sectionStyle = css`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`
+
+const headingStyle = css`
+  font-size: 25px;
+`
+
+const detailsStyle = css`
   display: flex;
   flex-direction: column;
   ${media.md} {
@@ -15,7 +33,7 @@ const baseDetailsStyle = css`
   }
 `
 
-const baseImageStyle = css`
+const imageStyle = css`
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -32,30 +50,47 @@ const baseImageStyle = css`
 `
 
 const PokemonDetails = ({ pokemon }) => {
-  console.log("pokemon", pokemon)
+  const { id, img, name, sprites, types } = pokemon
+  const profileImage = img ?? sprites.front_default
 
-  const profileImage = pokemon.img ?? pokemon.sprites.front_default
+  // Get random color based on pokemon id
+  const flag = id % 3
+  const { light } = useColors(flag)
+
+  // Set body color dynamically based on pokemon id
+  const bodyStyle = css`
+    body {
+      background-color: ${light};
+    }
+  `
+
+  console.log("pokemon", pokemon)
 
   return (
     <If condition={pokemon && typeof pokemon === "object"}>
-      <div css={baseDetailsStyle}>
-        <div className="profile">
-          <div>
-            <div css={baseImageStyle}>
-              <Image
-                src={profileImage}
-                alt={pokemon.name}
-                width={200}
-                height={200}
-              />
+      <Global styles={bodyStyle} />
+      <div css={detailsStyle}>
+        <div className="profile" css={columnStyle}>
+          <header css={columnStyle}>
+            <section className="pokemon-image" css={imageStyle}>
+              <Image src={profileImage} alt={name} width={200} height={200} />
               <span className="shadow"></span>
-            </div>
-            <div>
-              <span>{`#${String(pokemon.id).padStart(3, "0")}`}</span>
-              <Heading level={1}>{pokemon.name}</Heading>
-            </div>
-          </div>
-          <div>tes</div>
+            </section>
+            <section className="pokemon-name" css={sectionStyle}>
+              <span css={headingStyle}>
+                {`#${String(id).padStart(3, "0")}`}
+              </span>
+              <Heading level={1} css={headingStyle}>
+                {name}
+              </Heading>
+            </section>
+          </header>
+          <main>
+            <section className="pokemon-type" css={sectionStyle}>
+              <Heading level={2}>Type :</Heading>
+              <PokemonTypeList pokemonTypes={types} />
+            </section>
+          </main>
         </div>
         <div className="stats">stats</div>
       </div>
