181d10df1a5e54b68b613da883f7bfa4a94e0693
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-23 13:33:16 +0700

    🧹Refactor: Graphql client now uses cross-fetch

--- graphql/client.js
+++ graphql/client.js
@@ -1,6 +1,10 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client"
+import fetch from "cross-fetch"
 import { URI } from "./constants"
 
-const client = new ApolloClient({ uri: URI, cache: new InMemoryCache() })
+const client = new ApolloClient({
+  link: new HttpLink({ uri: URI, fetch }),
+  cache: new InMemoryCache(),
+})
 
 export default client
