7884136af30ee04bc4edc4ced86aa71006228619
Edwin Honami Hadiputra <edwinhonami11@yahoo.com>
2022-01-23 15:33:50 +0700

    🧹Refactor: amp support for pokemon-card

--- components/pokemon-card.js
+++ components/pokemon-card.js
@@ -109,7 +109,7 @@ const miniCardStyle = css`
 
 const PokemonCard = ({ pokemon }) => {
   const router = useRouter()
-  const { ownedPokemons } = useAppContext()
+  const { ownedPokemons, isAmp } = useAppContext()
 
   const isCollectionPage = router?.pathname === "/collection"
 
@@ -170,7 +170,22 @@ const PokemonCard = ({ pokemon }) => {
               </If>
             </header>
             <main css={mainStyle}>
-              <Image src={image} alt={pokemon.name} width={100} height={100} />
+              <If condition={!isAmp}>
+                <Image
+                  src={image}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+              </If>
+              <If condition={isAmp}>
+                <amp-img
+                  src={image}
+                  alt={pokemon.name}
+                  width={100}
+                  height={100}
+                />
+              </If>
               <Heading level={2} css={headingStyle}>
                 <span>
                   <If condition={pokemon.nickname !== undefined}>
